9b809c320699fc84be07f21743d1bd978c320e47
Glen Cheney <cheney.glen@gmail.com>
2025-06-14 20:26:30 -0400

    Add demo pages with CodePen embeds (#639)
    * Add demo pages with CodePen embeds
    
    * Fix md list list spacing
    
    * Fix lint

---
+++ apps/website/docs/demos/index.md
@@ -0,0 +1,19 @@
+---
+---
+
+# Demos
+
+There is [a collection of CodePen demos](https://codepen.io/collection/AWGLbd). They're also embedded in these pages:
+
+- [basic layout](./basic-layout.mdx)
+- [homepage demo](./homepage-demo.mdx)
+- [compound filters](./compound-filters.mdx)
+- [images](./images.mdx)
+- [image problems](./image-problems.mdx)
+- [adding and removing](./adding-and-removing.mdx)
+- [animated viewport entry](./animated-viewport-entry.mdx)
+- [bootstrap 3 grid](./bootstrap-3-grid.mdx)
+- [bootstrap 4 grid](./bootstrap-4-grid.mdx)
+- [with react](./react.mdx)
+- [async content](./async-content.mdx)
+- [Shuffle v5 with IE9](https://codepen.io/Vestride/details/dvExbg)
---
+++ apps/website/src/components/CodePenEmbed.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import styles from './CodePenEmbed.module.css';
+
+const author = 'Glen Cheney';
+
+export function CodePenEmbed({ penTitle, penId, penUser }) {
+  return (
+    <div>
+      <iframe
+        height={500}
+        className={styles.iframe}
+        scrolling="no"
+        title={penTitle}
+        src={`https://codepen.io/${penUser}/embed/${penId}?default-tab=result`}
+        loading="eager"
+        // eslint-disable-next-line react/no-unknown-property
+        allowtransparency="true"
+        allowFullScreen
+      >
+        See the Pen <a href={`https://codepen.io/${penUser}/pen/${penId}`}>{penTitle}</a> by {author} (
+        <a href={`https://codepen.io/${penUser}`}>@{penUser}</a>) on <a href="https://codepen.io">CodePen</a>.
+      </iframe>
+    </div>
+  );
+}
